e3de51e8966281a70b257d04e79ed422b7c492b7
TanguyFox <tanguyfox@gmail.com>
2023-12-08 15:16:22 +0100

    reset main

--- src/MainMLP.java
+++ src/MainMLP.java
@@ -2,30 +2,5 @@ import sae.mlp.MLP;
 
 public class MainMLP {
 
-    public static void main(String[] args) {
-        int[][] table = {
-            {0, 0, 0},
-            {0, 1, 0},
-            {1, 0, 0},
-            {1, 1, 1}
-        };
-
-        int[] layers = {2};
-        double learningRate = 0.5;
-
-        MLP mlp = new MLP(layers, learningRate, new sae.mlp.SigmoideFunction());
-
-        int maxPassage = 100;
-        int nbPassage = 0;
-        boolean appris = false;
-
-        while(nbPassage < maxPassage && !appris) {
-            for(int i = 0; i < table.length; i++) {
-                double[] input = {table[i][0], table[i][1]};
-                double[] output = {table[i][2]};
-                mlp.train(input, output);
-            }
-            nbPassage++;
-        }
-    }
+    public static void main(String[] args) {}
 }
